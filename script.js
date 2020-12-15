7cdea8476c275f9e1b7b076050d1c70b8cf0612f
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-12-15 19:10:50 +0000

    Initial commit - created team object literal.

---
+++ script.js
@@ -0,0 +1,6 @@
+// Create and extract information about a sports team. Once created, manipulate these data structures as well as gain insights from them. For example, get the total number of games the team has played, or the average score of all of their games.
+
+const team = {
+  _players: [],
+  _games: [],
+};
